1db8821af8a2a615a92a31b3924e69eba971bd4b
Mark Chavez <markchav3z@gmail.com>
2017-08-11 19:48:37 +0800

    refactor request

--- lib/ex_swapi/request.ex
+++ lib/ex_swapi/request.ex
@@ -3,11 +3,9 @@ defmodule ExSwapi.Request do
   def get(url) do
     HTTPoison.get(url)
     |> parse_response
-  end
-
-  defp parse_response({:ok, %HTTPoison.Response{status_code: _, body: body}}) do
-    body
     |> JSON.decode()
   end
+
+  defp parse_response({:ok, %HTTPoison.Response{status_code: _, body: body}}), do: body
 
 end
