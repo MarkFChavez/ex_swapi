eb417834ca71cb9e69c3c0756543e8cc1dba7e6c
Mark Chavez <markchav3z@gmail.com>
2017-08-11 18:57:40 +0800

    change variable name

--- lib/ex_swapi/request.ex
+++ lib/ex_swapi/request.ex
@@ -5,12 +5,12 @@ defmodule ExSwapi.Request do
     |> parse_response
   end
 
-  defp parse_response(_response = {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+  defp parse_response(_success = {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
     body
     |> JSON.decode()
   end
 
-  defp parse_response(_response = {:ok, %HTTPoison.Response{status_code: _, body: body}}) do
+  defp parse_response(_error = {:ok, %HTTPoison.Response{status_code: _, body: body}}) do
     # error
     IO.puts(body)
   end
