9c72c9295984229ba1aa86718f95db4c768cd80d
Mark <markchav3z@gmail.com>
2017-08-11 13:36:34 +0800

    initial commit

---
+++ lib/ex_swapi.ex
@@ -0,0 +1,18 @@
+defmodule ExSwapi do
+  @moduledoc """
+  Documentation for ExSwapi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSwapi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
