a1ae591cc8c29a8880689364bfd0035912dce426
Mark Chavez <markchav3z@gmail.com>
2017-08-11 19:12:36 +0800

    working find()

--- lib/ex_swapi.ex
+++ lib/ex_swapi.ex
@@ -1,13 +1,8 @@
 defmodule ExSwapi do
 
-  @moduledoc """
-  planets        = ExSwapi.all(:planets)
-  planet         = ExSwapi.find(:planet, 1)
-  search_results = ExSwapi.search(:planets, "search query")
-  """
-
   alias ExSwapi.Impl
 
-  defdelegate all(resources), to: Impl
+  defdelegate all(resource),      to: Impl
+  defdelegate find(resource, id), to: Impl
 
 end
--- lib/ex_swapi/impl.ex
+++ lib/ex_swapi/impl.ex
@@ -12,12 +12,21 @@ defmodule ExSwapi.Impl do
 
   alias ExSwapi.Request
 
+  # ALL RESOURCES
   def all(:films),      do: @films_uri |> make_request
   def all(:people),     do: @people_uri |> make_request
   def all(:planets),    do: @planets_uri |> make_request
   def all(:species),    do: @species_uri |> make_request
   def all(:vehicles),   do: @vehicles_uri |> make_request
   def all(:starships),  do: @starships_uri |> make_request
+
+  # RESOURCE BY ID
+  def find(:films,     id), do: "#{@films_uri}#{id}/"     |> make_request
+  def find(:people,    id), do: "#{@people_uri}#{id}/"    |> make_request
+  def find(:planets,   id), do: "#{@planets_uri}#{id}/"   |> make_request
+  def find(:species,   id), do: "#{@species_uri}#{id}/"   |> make_request
+  def find(:vehicles,  id), do: "#{@vehicles_uri}#{id}/"  |> make_request
+  def find(:starships, id), do: "#{@starships_uri}#{id}/" |> make_request
 
   defp make_request(uri), do: Request.get(uri)
 
