686ed3b04bcee19403334aaffb5f8ed7fc695466
Mark Chavez <markchav3z@gmail.com>
2017-08-11 18:37:57 +0800

    remove HTTPoison.start

--- lib/ex_swapi/request.ex
+++ lib/ex_swapi/request.ex
@@ -1,14 +1,8 @@
 defmodule ExSwapi.Request do
 
   def get(url) do
-    start_http()
-
     HTTPoison.get(url)
     |> parse_response
-  end
-
-  defp start_http do
-    HTTPoison.start
   end
 
   defp parse_response(_response = {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
