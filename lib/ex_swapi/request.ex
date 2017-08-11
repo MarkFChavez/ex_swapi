3b28d89d32a48e3c29f16e195d076965f13bc8bd
Mark Chavez <markchav3z@gmail.com>
2017-08-11 19:00:40 +0800

    remove unnecessary function

--- lib/ex_swapi/request.ex
+++ lib/ex_swapi/request.ex
@@ -5,14 +5,9 @@ defmodule ExSwapi.Request do
     |> parse_response
   end
 
-  defp parse_response(_success = {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+  defp parse_response({:ok, %HTTPoison.Response{status_code: _, body: body}}) do
     body
     |> JSON.decode()
-  end
-
-  defp parse_response(_error = {:ok, %HTTPoison.Response{status_code: _, body: body}}) do
-    # error
-    IO.puts(body)
   end
 
 end
