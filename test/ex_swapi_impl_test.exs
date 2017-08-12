a9b382018f06d46501e9de5ec1207f459ba1728a
Arman Jon Villalobos <me@armanjon.com>
2017-08-12 10:16:42 +0800

    Add test for ExSwapi.Impl#find/1 && ExSwapi.Impl#search/1

--- test/ex_swapi_impl_test.exs
+++ test/ex_swapi_impl_test.exs
@@ -9,4 +9,18 @@ defmodule ExSwapiImplTest do
     assert status == :ok
     assert Map.has_key?(response, "results") == true
   end
+
+  test "find returns proper response" do
+    {status, response} = Impl.find(:people, 10)
+
+    assert status == :ok
+    assert Map.has_key?(response, "birth_year")
+  end
+
+  test "search returns proper response" do
+    {status, response} = Impl.search(:people, "Obi")
+
+    assert status == :ok
+    assert Map.has_key?(response, "results")
+  end
 end
